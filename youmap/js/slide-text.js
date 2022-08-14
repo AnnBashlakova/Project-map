d29d49bfc601d7ba5f5a33c073fdbac5150f31d9
Anna <anna47297@gmail.com>
2022-08-14 14:33:34 +0300

    feat: add animatin-scroll

--- youmap/js/slide-text.js
+++ youmap/js/slide-text.js
@@ -4,11 +4,11 @@ const p = post.length;
 let i = 0;
 setInterval(next, 2000);
 
-post[0].style = "opacity:1";
-post[1].style = "opacity:0";
-post[2].style = "opacity:0";
-post[3].style = "opacity:0";
-post[4].style = "opacity:0";
+// post[0].style = "opacity:1";
+// post[1].style = "opacity:0";
+// post[2].style = "opacity:0";
+// post[3].style = "opacity:0";
+// post[4].style = "opacity:0";
 
 
 function next() {
@@ -18,25 +18,18 @@ function next() {
     post[p - 1].style = "top:100px;z-index:1;opacity:0";
 
     post[i].style = "top:0;z-index:3";
-    
     i++;
+
     } else if (i > 0 && i < p - 1) {
     post[i - 1].style = "top:100px;z-index:1;opacity:0";
     post[i + 1].style = "top:-100px;z-index:1;opacity:0";
 
     post[i].style = "top:0;z-index:3";
-    
 
-    
-    
     i++;
     } else if (i === p - 1) {
     post[i - 1].style = "top:100px;z-index:1;opacity:0";
-    
-
     post[i].style = "top:0;z-index:3";
-    
-
     i = 0;
 
     post[i].style = "top:-100px;z-index:1;opacity:0";
@@ -46,38 +39,30 @@ function next() {
 
 
 
-
-const phone = document.querySelector('.right_part_img')
-// console.log(document.scrollTop());
-
-
+const phone = document.querySelector('.right_part_img');
 
 
 window.addEventListener('scroll', scrollPhone);
 function scrollPhone() {
     let vertical_position = pageYOffset;
-    // console.log(vertical_position);
-    if(vertical_position > 2410){
-        console.log('dfvdfdf');
+    if(vertical_position > 2267){
         phone.classList.remove('stick');
         phone.classList.add('position');
-        // phone.style.transform= "translateY(361%)";
+
     } else {
         phone.classList.add('stick');
         phone.classList.remove('position');
-        // phone.style.transform = "translate(236%, -18%)";
-    
-        // phone.classList.add('pos');
+
     }
 };
 
 
-const accordionContents = document.getElementsByClassName("accordion__content");
-for (i = 0; i < accordionContents.length; i++) {
-    accordionContents[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    });
-}
+// const accordionContents = document.getElementsByClassName("accordion__content");
+// for (i = 0; i < accordionContents.length; i++) {
+//     accordionContents[i].addEventListener("click", function () {
+//     this.classList.toggle("active");
+//     });
+// }
 
 
 // const plus = document.getElementsByClassName("img-plus");
